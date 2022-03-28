bfd1dd2c7307e583e5021abaa6e1a3d3ac7a3157
appleRui <apple31581@gmail.com>
2022-03-28 15:48:47 +0900

    Feature/#18

--- resources/views/shops/new.blade.php
+++ resources/views/shops/new.blade.php
@@ -9,19 +9,27 @@
       <input type="text" name="name" class="form-control">
     </div>
     <div>
-      <label for="datetime-local">場所</label>
-      <select class="form-control" name="prefecture_id">
-        @foreach($prefectures as $prefecture)
-        <option value="{{ $prefecture['id'] }}">{{ $prefecture['prefecture'] }}</option>
+      <label class="mt-3" for="genre">ジャンル</label>
+      <select class="form-control" name="genre_id">
+        @foreach($genres as $genre)
+        <option value="{{ $genre['id'] }}">{{ $genre['name'] }}</option>
         @endforeach
       </select>
     </div>
     <div>
-      <label for="datetime-local">説明</label>
+      <label class="mt-3" for="prefecture">場所</label>
+      <select class="form-control" name="prefecture_id">
+        @foreach($prefectures as $prefecture)
+        <option value="{{ $prefecture['id'] }}">{{ $prefecture['prefecture'] }}</option>
+        @endforeach
+    </select>
+    </div>
+    <div>
+      <label class="mt-3" for="description">説明</label>
       <textarea type="text" name="description" class="form-control"></textarea>
     </div>
     <div>
-      <label for="datetime-local">店舗画像</label>
+      <label class="mt-3" for="shop_img">店舗画像</label>
       <input type="file" id="image" name="image" class="form-control">
     </div>
     <input type="submit" class="mt-3 btn btn-primary" value="送信する"></input>
